e06be0310db8fce15d48e46890bb9427fe8e4132
“wiatec” <patrickxu@wiatec.com>
2020-04-24 14:10:38 +0800

    202004241410

--- app/src/main/java/com/ex/player/MainActivity.java
+++ app/src/main/java/com/ex/player/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.ex.lib.security.RSA;
 import com.ex.libplayer.controller.ExPlayerController;
 import com.ex.libplayer.player.Player;
 import com.ex.libplayer.view.ExPlayView;
